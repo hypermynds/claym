92f6903a0c32e5ea17cc8ad1f37416683a6ade75
Mattia Penati <mp@hypermynds.com>
2022-07-16 00:06:30 +0200

    assert_le

--- src/lib.rs
+++ src/lib.rs
@@ -12,6 +12,7 @@
 //!
 //! * [`assert_ge!`]
 //! * [`assert_gt!`]
+//! * [`assert_le!`]
 //! * [`assert_matches!`]
 //!
 //! Assertions for [`Result`] variants:
@@ -29,6 +30,7 @@
 mod assert_err;
 mod assert_ge;
 mod assert_gt;
+mod assert_le;
 mod assert_matches;
 mod assert_none;
 mod assert_ok;
